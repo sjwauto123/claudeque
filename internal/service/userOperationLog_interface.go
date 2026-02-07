1de202c3d79a8b38a1c2caefaf1f1238ed759b07
st <2663600842@qq.com>
2026-02-07 19:27:05 +0800

    perf:总结同步websocket连接池，实现自测获取用户日志信息功能。

--- internal/service/userOperationLog_interface.go
+++ internal/service/userOperationLog_interface.go
@@ -9,5 +9,6 @@ type UserOperationLogService interface {
 	GetUserLogs(r *request.UserLogsRequest, start time.Time, end time.Time) (interface{}, error)
 
 	CreateLog(username, actionType, object, description string, success bool) error
+
 	LimitLogs(limit int64) error
 }
