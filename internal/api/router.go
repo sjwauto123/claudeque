bac8c011cd18e7a3932a9286b929488a93dda616
spring <2144515062@qq.com>
2026-02-24 11:03:46 +0800

    feat：修改了路径

--- internal/api/router.go
+++ internal/api/router.go
@@ -46,13 +46,6 @@ func (r *Router) Setup(engine *gin.Engine) {
 		})
 	})
 
-	engine.GET("/api/v1/buildtest", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"status":  "ok",
-			"message": "/api/v1/buildtestabc123456",
-		})
-	})
-
 	// API v1 路由组
 	v1 := engine.Group("/api/v1")
 	{
