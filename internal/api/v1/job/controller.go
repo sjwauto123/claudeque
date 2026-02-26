d9d96d5b53a49aa23927971d18016d6956ba4de5
Rfh <2129905621@qq.com>
2026-02-26 21:51:09 +0800

    feat:修改前端传来的时间处理

--- internal/api/v1/job/controller.go
+++ internal/api/v1/job/controller.go
@@ -48,13 +48,13 @@ func (ctrl *Controller) handleJobList(c *gin.Context, fetchFunc func(request.Job
 		return
 	}
 	userID := middleware.GetUserID(c)
-	startTime, err := utils.ParseDateToStart(j.StartTime)
+	startTime, err := utils.ParseStartDate(j.StartTime)
 	if err != nil {
 		response.BadRequest(c, "开始时间格式错误")
 		return
 	}
 
-	endTime, err := utils.ParseDateToEnd(j.EndTime)
+	endTime, err := utils.ParseEndDate(j.EndTime)
 	if err != nil {
 		response.BadRequest(c, "截至时间格式错误")
 		return
